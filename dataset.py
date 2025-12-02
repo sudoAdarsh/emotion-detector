29e2b34a56da8a0bd81cb203e441b20dc071b06c
sudoAdarsh <upadhyayadarsh1234a@gmail.com>
2025-12-02 20:12:01 +0530

    Formatted Dataset for pytorch to use

---
+++ dataset.py
@@ -0,0 +1,14 @@
+import torch
+
+class EmotionDataset(torch.utils.data.Dataset):
+    def __init__(self, encodings, labels):
+        self.encodings = encodings
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.labels)
+
+    def __getitem__(self, idx):
+        item = {key: val[idx] for key, val in self.encodings.items()}
+        item['labels'] = torch.tensor(self.labels[idx], dtype=torch.long)
+        return item
